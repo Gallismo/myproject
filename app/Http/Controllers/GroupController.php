a33f3986a92809f4512fc3734ec8b741146507e1
Gallismo <yourrussianweapon@gmail.com>
2022-07-06 20:35:40 +0500

    Group optimized

--- app/Http/Controllers/GroupController.php
+++ app/Http/Controllers/GroupController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\GroupFormRequest;
 use App\Models\Department;
 use App\Models\Group;
 use App\Models\Teacher;
@@ -10,21 +11,10 @@ use Illuminate\Support\Facades\Validator;
 
 class GroupController extends Controller
 {
-    public function createGroup (Request $request) {
-        $val = Validator::make($request->all(), [
-            'name' => 'required|unique:groups|string|max:14',
-            'department_code' => 'required|string',
-            'start_year' => 'required|string|min:4|max:4',
-            'end_year' => 'required|string|min:4|max:4'
-        ]);
+    public function createGroup (GroupFormRequest $req) {
+        $request = $req->validated();
 
-        if ($val->fails()) {
-            return response()->json(['title' => 'Ошибка валидации',
-                'text' => 'Проверьте правильность заполнения полей',
-                'errors' => $val->errors()], 422);
-        }
-
-        $val = Validator::make($request->all(), [
+        $val = Validator::make($request, [
             'start_year' => 'integer',
             'end_year' => 'integer'
         ]);
@@ -35,69 +25,38 @@ class GroupController extends Controller
                 'errors' => $val->errors()], 422);
         }
 
-        $department = Department::where('code', $request->department_code)->first();
+        $department = Department::where('code', $request['department_code'])->first();
 
-        if (!$department) {
-            return response()->json(['title' => 'Ошибка',
-                'text' => 'Такое отделение не существует',
-                'errors' => $val->errors()], 422);
-        }
         $code = $this->codeGenerate(Group::class);
         Group::create([
-            'name' => $request->name,
-            'department_id' => $department->id,
-            'start_year' => $request->start_year,
-            'end_year' => $request->end_year,
+            'name' => $request['name'],
+            'department_id' => $department['id'],
+            'start_year' => $request['start_year'],
+            'end_year' => $request['end_year'],
             'code' => $code
         ]);
 
         return response()->json(['title' => 'Успешно',
             'text' => 'Группа была успешно добавлена',
-            'errors' => $val->errors()], 200);
+            'errors' => new \stdClass()], 200);
     }
 
-    public function deleteGroup (Request $request) {
-        $val = Validator::make($request->all(), [
-            'code' => 'required|string'
-        ]);
+    public function deleteGroup (GroupFormRequest $req) {
+        $request = $req->validated();
 
-        if ($val->fails()) {
-            return response()->json(['title' => 'Ошибка валидации',
-                'text' => 'Проверьте правильность заполнения полей',
-                'errors' => $val->errors()], 422);
-        }
-
-        $group = Group::where('code', $request->code)->first();
-
-        if (!$group) {
-            return response()->json(['title' => 'Ошибка',
-                'text' => 'Группа не существует',
-                'errors' => $val->errors()], 422);
-        }
+        $group = Group::where('code', $request['code'])->first();
 
         $group->delete();
 
         return response()->json(['title' => 'Успешно',
             'text' => 'Группа была успешно удалена',
-            'errors' => $val->errors()], 200);
+            'errors' => new \stdClass()], 200);
     }
 
-    public function editGroup (Request $request) {
-        $val = Validator::make($request->all(), [
-            'code' => 'required|string',
-            'name' => 'required_without_all:start_year,end_year,department_code|string|unique:groups,name|max:14',
-            'department_code' => 'required_without_all:start_year,end_year,name|string',
-            'start_year' => 'required_without_all:department_code,end_year,name|string|min:4|max:4',
-            'end_year' => 'required_without_all:start_year,department_code,name|string|min:4|max:4'
-        ]);
+    public function editGroup (GroupFormRequest $req) {
+        $request = $req->validated();
 
-        if ($val->fails()) {
-            return response()->json(['title' => 'Ошибка валидации',
-                'text' => 'Проверьте правильность заполнения полей',
-                'errors' => $val->errors()], 422);
-        }
-
-        $val = Validator::make($request->all(), [
+        $val = Validator::make($request, [
             'start_year' => 'integer',
             'end_year' => 'integer'
         ]);
@@ -108,26 +67,16 @@ class GroupController extends Controller
                 'errors' => $val->errors()], 422);
         }
 
-        $group = Group::where('code', $request->code)->first();
+        $group = Group::where('code', $request['code'])->first();
 
-        if (!$group) {
-            return response()->json(['title' => 'Ошибка',
-                'text' => 'Группа не существует',
-                'errors' => $val->errors()], 422);
-        }
-        if ($request->department_code) {
-            $department = Department::where('code', $request->department_code)->first();
-            if (!$department) {
-                return response()->json(['title' => 'Ошибка',
-                    'text' => 'Такого отделения не существует',
-                    'errors' => $val->errors()], 422);
-            }
+        if (isset($request['department_code'])) {
+            $department = Department::where('code', $request['department_code'])->first();
             $group->department_id = $department->id;
         }
 
-        $request->name ? $group->name = $request->name : false;
-        $request->start_year ? $group->start_year = $request->start_year : false;
-        $request->end_year ? $group->end_year = $request->end_year : false;
+        isset($request['name']) ? $group->name = $request['name'] : false;
+        isset($request['start_year']) ? $group->start_year = $request['start_year'] : false;
+        isset($request['end_year']) ? $group->end_year = $request['end_year'] : false;
 
         $group->save();
 
