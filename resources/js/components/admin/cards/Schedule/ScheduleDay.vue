33021e02c44dc21b0b249c2cb912e08e628024f7
gosha <galilovgeorgij@gmail.com>
2022-09-16 12:03:34 +0300

    raspisanie_m1

---
+++ resources/js/components/admin/cards/Schedule/ScheduleDay.vue
@@ -0,0 +1,58 @@
+<template >
+    <div class="card bg-dark text-white">
+    <div class="card-body">
+    <h5 class="card-title">
+        Понедельник
+    </h5>
+        <div class="card bg-dark text-white">
+            <div class="card-body">
+                <h5 class="card-title">
+                    1 Пара
+                </h5>
+                <p class="card-text">
+                    9:00-9:45
+               <img src="/img/break.svg" class="card-img-break">
+                    10м
+                </p>
+            </div>
+        </div>
+         <div class="card bg-dark text-white">
+            <div class="card-body">
+                <h5 class="card-title">
+                    1 Пара
+                </h5>
+                <p class="card-text">
+                    9:00-9:45
+               <img src="/img/break.svg" class="card-img-break">
+                    10м
+                </p>
+            </div>
+        </div>
+         <div class="card bg-dark text-white">
+            <div class="card-body">
+                <h5 class="card-title">
+                    1 Пара
+                </h5>
+                <p class="card-text">
+                    9:00-9:45
+               <img src="/img/break.svg" class="card-img-break">
+                    10м
+                </p>
+            </div>
+        </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style>
+    .card-img-break{
+        width: 16px;
+        height: 16px;
+        position: relative;
+        top:-1px;
+    }
+</style>
